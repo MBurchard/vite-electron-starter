03e806e1316e224398d74386514d1bf272a461c3
Martin Burchard <dev@burchard-it.de>
2025-01-21 17:38:41 +0100

    add some useful functionality for the preload.ts

--- modules/electron-preload/src/preload.ts
+++ modules/electron-preload/src/preload.ts
@@ -1,12 +1,50 @@
+import type {Versions} from '@common/definitions.js';
 import {getLog} from '@common/logging.js';
-import {contextBridge} from 'electron';
+import {contextBridge, ipcRenderer} from 'electron';
 
 const log = getLog('electron.preload');
 
-contextBridge.exposeInMainWorld('backend', {
-  doSthElse: () => {
-    log.debug('do something else');
+/**
+ * Request some data/information from the Electron main process.
+ *
+ * @param {string} channel
+ * @param args
+ * @return {Promise<*>}
+ */
+async function invoke<T>(channel: string, ...args: any[]): Promise<T> {
+  return ipcRenderer.invoke(channel, ...args);
+}
+
+/**
+ * Send some data/information to the Electron main process.
+ *
+ * @param {string} channel
+ * @param args
+ */
+function emit(channel: string, ...args: any[]): void {
+  ipcRenderer.send(channel, ...args);
+}
+
+export interface Backend {
+  emit: (channel: string, ...args: any[]) => void;
+  getVersions: () => Promise<Versions>;
+  invoke: <T>(channel: string, ...args: any[]) => Promise<T>;
+}
+
+const backend: Backend = {
+  emit,
+  getVersions: async () => {
+    return invoke('getVersions');
   },
-});
+  invoke,
+};
+
+declare global {
+  interface Window {
+    backend: Backend;
+  }
+}
+
+contextBridge.exposeInMainWorld('backend', backend);
 
 log.debug('Preload JS prepared');
