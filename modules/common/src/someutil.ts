5e495882328a1f6e4ef3d20bfb2fb891f254793a
Martin Burchard <dev@burchard-it.de>
2025-02-03 16:56:14 +0100

    remove demo code

--- modules/common/src/someutil.ts
+++
@@ -1,8 +0,0 @@
-import {getLog} from '@common/logging.js';
-
-const log = getLog('common.some.util');
-
-export function doSth(param: string): string {
-  log.debug('doSth has been used');
-  return `Hallo ${param}`;
-}
