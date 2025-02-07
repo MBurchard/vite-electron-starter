ddcf35c62efab0e000b27ecdd007692b9c27e4fc
Martin Burchard <dev@burchard-it.de>
2025-02-07 01:34:37 +0100

    define IpcChannels and enhance the Electron Display with property primary

--- modules/common/src/definitions.ts
+++ modules/common/src/definitions.ts
@@ -1,4 +1,20 @@
-export type IpcChannel = 'frontend-logging' | 'getVersions' | 'show-demo-popup';
+export interface Display extends Electron.Display {
+  primary: boolean;
+}
+
+export type IpcChannel =
+  // logs messages from the frontend to the backend
+  'frontendLogging' |
+  // request display data from the backend
+  'getDisplayData' |
+  // Request version information (Electron, Node.js, Chrome).
+  'getVersions' |
+  // Trigger the opening of the display demo window.
+  'showDisplayDemo' |
+  // Sends updated display data from the backend to the frontend.
+  'updateDisplayData' |
+  // Notifies the backend that a window has fully loaded.
+  'windowFullyLoaded';
 
 export interface Versions {
   chrome: string;
