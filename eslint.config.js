0b2683b8ddc0a8c76c31c8275c98a5a4e25e0940
Martin Burchard <dev@burchard-it.de>
2025-05-23 17:08:56 +0200

    add changes to ESLint config from bit-log project

--- eslint.config.js
+++ eslint.config.js
@@ -9,18 +9,24 @@ export default antfu({
   },
 }, {
   rules: {
-    'curly': 'off',
+    curly: 'off',
     'regexp/strict': 'off',
     'style/block-spacing': ['error', 'never'],
     'style/brace-style': ['error', '1tbs'],
     'style/max-len': ['warn', {code: 120}],
     'style/object-curly-spacing': ['error', 'never'],
     'style/operator-linebreak': ['error', 'after'],
+    'style/quote-props': ['error', 'as-needed', {unnecessary: true}],
   },
 }, {
   files: ['**/*.md'],
   rules: {
     'style/no-trailing-spaces': 'off',
+    'style/max-len': 'off',
+  },
+}, {
+  files: ['**/package.json'],
+  rules: {
     'style/max-len': 'off',
   },
 });
